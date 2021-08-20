dfff88151b63d9b2be5f6af2195b47eae80a8a55
tropicoo <tropicoo@users.noreply.github.com>
2021-08-20 20:49:50 +0300

    Add validation for recorded video file

--- hikcamerabot/services/tasks/video.py
+++ hikcamerabot/services/tasks/video.py
@@ -29,32 +29,61 @@ class RecordVideoTask:
         self._log = logging.getLogger(self.__class__.__name__)
         self._conf = conf
         self._cam = cam
+        self._bot: 'CameraBot' = cam.bot
         self._video_type = video_type
         self._file_path = os.path.join(storage_path, self._get_filename())
         self._ffmpeg_cmd_full = f'{ffmpeg_cmd} {self._file_path}'
         self._context = context
+        self._event = self._video_type_to_event[self._video_type]
 
     async def run(self) -> None:
-        await self._record()
-        await self._send_result()
+        if await self._record():
+            await self._send_result()
 
-    async def _send_result(self):
-        await get_result_queue().put({
-            'event': self._video_type_to_event[self._video_type],
-            'video_path': self._file_path,
-            'cam': self._cam,
-            'message': self._context
-        })
-
-    async def _record(self) -> None:
+    async def _record(self) -> bool:
         """Start Ffmpeg subprocess and return file path and video type."""
         self._log.debug('Recording video gif from %s: %s',
                         self._conf.description, self._ffmpeg_cmd_full)
         await self._start_ffmpeg_subprocess()
+        validated = await self._validate_file()
+        if not validated:
+            err_msg = f'Failed to record {self._file_path}'
+            self._log.error(err_msg)
+            await self._bot.send_message(
+                self._context.chat.id,
+                text=f'{err_msg}.\nEvent type: {self._event}\nCheck logs.',
+                reply_to_message_id=self._context.message_id if self._context else None,
+            )
+        return validated
 
     async def _start_ffmpeg_subprocess(self) -> None:
         proc = await asyncio.create_subprocess_shell(self._ffmpeg_cmd_full)
         await proc.wait()
+
+    async def _validate_file(self) -> bool:
+        """Validate recorded file existence and size."""
+        try:
+            is_empty = os.path.getsize(self._file_path) == 0
+        except FileNotFoundError:
+            self._log.error('Failed to validate %s: File does not exist',
+                            self._file_path)
+            return False
+        except Exception:
+            self._log.exception('Failed to validate %s', self._file_path)
+            return False
+
+        if is_empty:
+            self._log.error('Failed to validate %s: File %s is empty',
+                            self._file_path)
+        return not bool(is_empty)
+
+    async def _send_result(self):
+        await get_result_queue().put({
+            'event': self._event,
+            'video_path': self._file_path,
+            'cam': self._cam,
+            'message': self._context
+        })
 
     def _get_filename(self) -> str:
         return self._video_filename[self._video_type].format(
