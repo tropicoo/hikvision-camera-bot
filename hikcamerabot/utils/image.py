8fda4cd9a834077c5a4b35e1296294fef1ea44f7
tropicoo <tropicoo@users.noreply.github.com>
2023-12-09 22:40:45 +0200

    Correctly resize 4x3 cameras #122

--- hikcamerabot/utils/image.py
+++ hikcamerabot/utils/image.py
@@ -20,10 +20,11 @@ class ImageProcessor(metaclass=Singleton):
         """Return resized JPEG snapshot."""
         self._log.debug('Resizing snapshot')
         ImageFile.LOAD_TRUNCATED_IMAGES = True
-        raw_snapshot = Image.open(raw_snapshot)
+        raw_snapshot_image = Image.open(raw_snapshot)
         resized_snapshot = BytesIO()
 
-        snapshot: Image.Image = raw_snapshot.resize(Img.SIZE, Image.LANCZOS)
+        size = self._calculate_size(raw_snapshot_image)
+        snapshot: Image.Image = raw_snapshot_image.resize(size, Image.LANCZOS)
         snapshot.save(
             resized_snapshot,
             Img.FORMAT,
@@ -34,9 +35,25 @@ class ImageProcessor(metaclass=Singleton):
 
         self._log.debug(
             'Raw snapshot: %s, %s, %s',
-            raw_snapshot.format,
-            raw_snapshot.mode,
-            raw_snapshot.size,
+            raw_snapshot_image.format,
+            raw_snapshot_image.mode,
+            raw_snapshot_image.size,
         )
-        self._log.debug('Resized snapshot: %s', Img.SIZE)
+        self._log.debug('Resized snapshot: %s', size)
         return resized_snapshot
+
+    def _calculate_size(self, raw_snapshot_image: Image.Image) -> tuple[int, int]:
+        """Make it work correctly for 4x3 cameras by JulyIghor.
+        https://github.com/tropicoo/hikvision-camera-bot/issues/122
+        """
+        # Calculate new size maintaining aspect ratio
+        target_width, target_height = Img.SIZE
+        aspect_ratio = raw_snapshot_image.width / raw_snapshot_image.height
+
+        # Decide if the image should be scaled based on the target width or height
+        if (
+            raw_snapshot_image.width / target_width
+            < raw_snapshot_image.height / target_height
+        ):
+            return int(target_height * aspect_ratio), target_height
+        return target_width, int(target_width / aspect_ratio)
